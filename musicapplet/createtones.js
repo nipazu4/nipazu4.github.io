72d6cb0871680760e1994df70217a64e1b86ef26
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-17 14:44:06 +0200

    Update createtones.js

--- musicapplet/createtones.js
+++ musicapplet/createtones.js
@@ -1,7 +1,7 @@
 $('button').click(function() {
     $('.output').empty();
     
-    for (var a = [0, 1, 2, 3, 4], i = a.length; i--; ) {
+    for (var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], i = a.length; i--; ) {
         var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
         $('.output').append('<span>' + random + '</span>');
     }

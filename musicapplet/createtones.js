a19f77d7d543679d27528ab21ce19e20bb50e142
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-17 19:38:35 +0200

    Update createtones.js

--- musicapplet/createtones.js
+++ musicapplet/createtones.js
@@ -1,5 +1,5 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var i;
+var n;
 
 function shuffle(o) {
     for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
@@ -7,9 +7,9 @@ function shuffle(o) {
 };
 
 function printNumbers() {
-	for (i = 0; i < numbers.length; i++) {
-		var elementId = "output" + i;
-		document.getElementById(elementId).innerHTML = "<span>" + shuffle(numbers)[i] + "</span>";
+	for (n = 0; n < numbers.length; n++) {
+		var elementId = "output" + n;
+		document.getElementById(elementId).innerHTML = "<span>" + shuffle(numbers)[n] + "</span>";
 	}
 	
 }

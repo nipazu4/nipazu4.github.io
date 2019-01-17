3294d1bdbcaef9fcdbd257de5ca09327df2f8e19
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-17 18:43:44 +0200

    Update createtones.js

--- musicapplet/createtones.js
+++ musicapplet/createtones.js
@@ -1,4 +1,5 @@
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var i;
 
 function shuffle(o) {
     for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
@@ -6,16 +7,7 @@ function shuffle(o) {
 };
 
 function printNumbers() {
-	document.getElementById("output").innerHTML = shuffle(numbers)[0];
-	document.getElementById("output").innerHTML = shuffle(numbers)[1];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[2];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[3];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[4];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[5];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[6];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[7];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[8];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[9];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[10];	
-	document.getElementById("output").innerHTML = shuffle(numbers)[11];	
+	for (i = 0; i < numbers.length; i++) {
+		document.getElementById("output").innerHTML = shuffle(numbers)[i];
+	}
 }

3e6a64ad52a3c6df7576a278d728a96eac6884da
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-23 22:26:54 +0200

    .

--- musicapplet/flow.js
+++ musicapplet/flow.js
@@ -1,3 +1,22 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var n;
+
+function Shuffle(o) {
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
+
+function printNumbers() {
+	shuffledNumbers = Shuffle(numbers);
+	for (n = 0; n < numbers.length; n++) {
+		var elementId = "output" + n;
+		document.getElementById(elementId).innerHTML = shuffledNumbers[n];
+	}
+	return shuffledNumbers;
+}
+
+
+
 VF = Vex.Flow;
 
 // We created an object to store the information about the workspace

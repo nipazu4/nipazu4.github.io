0203d9fbdb45bf2fe2c54103673f298ef4f22744
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-23 22:06:01 +0200

    Update flow.js

--- musicapplet/flow.js
+++ musicapplet/flow.js
@@ -54,18 +54,18 @@ var notenames = [
 ]
 
 var notes = [
+    notenames[0],
     notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1],
-    notenames[1]
+    notenames[2],
+    notenames[3],
+    notenames[4],
+    notenames[5],
+    notenames[6],
+    notenames[7],
+    notenames[8],
+    notenames[9],
+    notenames[10],
+    notenames[11]
 ]
 
 var voice = new VF.Voice({num_beats: 12,  beat_value: 4});

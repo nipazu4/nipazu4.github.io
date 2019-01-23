dd7c7845e912cb8e2a57a10936a38f21495eedc2
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-23 18:00:52 +0200

    .

--- musicapplet/flow.js
+++ musicapplet/flow.js
@@ -1,20 +1,36 @@
 VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var canvas = document.getElementById("row")
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+// We created an object to store the information about the workspace
+var WorkspaceInformation = {
+    // The div in which you're going to work
+    div: document.getElementById("row"),
+    // Vex creates a svg with specific dimensions
+    canvasWidth: 600,
+    canvasHeight: 360
+};
 
-// Size our svg:
-renderer.resize(360, 600);
+// Create a renderer with SVG
+var renderer = new VF.Renderer(
+    WorkspaceInformation.div,
+    VF.Renderer.Backends.SVG
+);
 
-// And get a drawing context:
+// Use the renderer to give the dimensions to the SVG
+renderer.resize(WorkspaceInformation.canvasWidth, WorkspaceInformation.canvasHeight);
+
+// Expose the context of the renderer
 var context = renderer.getContext();
 
-// Create a stave at position 10, 40 of width 400 on the canvas.
-var stave = new VF.Stave(10, 40, 400);
+// And give some style to our SVG
+context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
+
+/**
+ * Creating a new stave
+ */
+// Create a stave of width 400 at position x10, y40 on the SVG.
+var stave = new VF.Stave(10, 40, 400);
 // Add a clef and time signature.
 stave.addClef("treble").addTimeSignature("4/4");
-
-// Connect it to the rendering context and draw!
+// Set the context of the stave our previous exposed context and execute the method draw !
 stave.setContext(context).draw();

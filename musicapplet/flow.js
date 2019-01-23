49a7eb78ab968f434121bb1ebecc129fb0173825
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-23 19:47:23 +0200

    Update flow.js

--- musicapplet/flow.js
+++ musicapplet/flow.js
@@ -1,18 +1,36 @@
 VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "boo".
-var vf = new Vex.Flow.Factory({
-    renderer: {
-        selector: 'row',
-        height: 160,
-        width: 590
-    }
-});
-var score = vf.EasyScore();
-var system = vf.System();
+// We created an object to store the information about the workspace
+var WorkspaceInformation = {
+    // The <div> element in which you're going to work
+    div: document.getElementById("row"),
+    // Vex creates a svg with specific dimensions
+    canvasWidth: 590,
+    canvasHeight: 160
+};
 
-system.addStave({
-  voices: [score.voice(score.notes('C#5/q, B4, A4, G#4'))]
-}).addClef('treble').addTimeSignature('4/4');
+// Create a renderer with SVG
+var renderer = new VF.Renderer(
+    WorkspaceInformation.div,
+    VF.Renderer.Backends.SVG
+);
 
-vf.draw();
+// Use the renderer to give the dimensions to the canvas
+renderer.resize(WorkspaceInformation.canvasWidth, WorkspaceInformation.canvasHeight);
+
+// Expose the context of the renderer
+var context = renderer.getContext();
+
+// And give some style to our canvas
+context.setFont("Lucida Console", 12, "").setBackgroundFillStyle("#eed");
+
+
+/**
+ * Creating a new stave
+ */
+// Create a stave of width 400 at position x10, y40 on the canvas.
+var stave = new VF.Stave(5, 40, 580);
+// Add a clef and time signature.
+stave.addClef("treble");
+// Set the context of the stave our previous exposed context and execute the method draw !
+stave.setContext(context).draw();

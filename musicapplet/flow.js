3fd8c8c86c1e201812af1138fdc982615ac46ba9
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-24 19:27:12 +0200

    Update flow.js

--- musicapplet/flow.js
+++ musicapplet/flow.js
@@ -1,18 +1,18 @@
 const VF = Vex.Flow;
 
 var numbers = [
-	{number:1, tone:"E4"},
-	{number:2, tone:"F4"},
-	{number:3, tone:"F#4"},
-	{number:4, tone:"G4"},
-	{number:5, tone:"G#4"},
-	{number:6, tone:"A4"},
-	{number:7, tone:"A#4"},
-	{number:8, tone:"B4"},
-	{number:9, tone:"C5"},
-	{number:10, tone:"C#5"},
-	{number:11, tone:"D5"},
-	{number:12, tone:"D#5"}
+	{number:1, tone:'E4'},
+	{number:2, tone:'F4'},
+	{number:3, tone:'F#4'},
+	{number:4, tone:'G4'},
+	{number:5, tone:'G#4'},
+	{number:6, tone:'A4'},
+	{number:7, tone:'A#4'},
+	{number:8, tone:'B4'},
+	{number:9, tone:'C5'},
+	{number:10, tone:'C#5'},
+	{number:11, tone:'D5'},
+	{number:12, tone:'D#5'}
 ];
 
 var n;
@@ -45,7 +45,7 @@ var system = vf.System({x: 110, y: 40, width: 475});
 
 system.addStave({
 	voices: [
-		score.voice(score.notes(shuffledVoices, {stem: 'up'}))
+		score.voice(score.notes(printNumbers(), {stem: 'up'}))
 	]
 });
 system2.addStave({

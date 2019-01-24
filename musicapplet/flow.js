28bd1cf8e087067743047326a1683e72a6f25ac4
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-24 16:51:00 +0200

    Update flow.js

--- musicapplet/flow.js
+++ musicapplet/flow.js
@@ -40,10 +40,9 @@ var score = vf.EasyScore();
 var system = vf.System();
 
 system.addStave({
-  voices: [
-    score.voice(score.notes('C#5/q, B4, A4, G#4', {stem: 'up'})),
-    score.voice(score.notes('C#4/h, C#4', {stem: 'down'}))
-  ]
+	voices: [
+		score.voice(score.notes('C#5/q, B4, A4, G#4', {stem: 'up'}))
+	]
 }).addClef('treble');
 
 vf.draw();

8871ccfd153b77562a6d6a733edb60e40becca06
nipazu4 <45739791+nipazu4@users.noreply.github.com>
2019-01-24 18:47:11 +0200

    Update flow.js

--- musicapplet/flow.js
+++ musicapplet/flow.js
@@ -50,7 +50,7 @@ system.addStave({
 });
 system2.addStave({
 	voices: [
-		score.voice(score.notes('B4/w, B4/w, B4/w', {stem: 'up'}))
+		score.voice(score.notes('B4/w/r, B4/w/r, B4/w/r', {stem: 'up'}))
 	]
 }).addClef('treble');
 

5164505c03aaf8c3136d807c664178e2d8343d2f
Niilo Saarlemo <nipazu4@gmail.com>
2020-12-03 17:20:46 +0200

    .

--- js/search.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("search").addEventListener("submit", searchQuery);
-
-function searchQuery() {
-	var inputvalue = document.getElementById("searchtextinput").value;
-	window.location.href = "http://stackoverflow.com";
-}
